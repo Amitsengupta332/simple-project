61821b00aaff2e964831b5a491ac60ec73e80a49
Amitsengupta332 <amitsengupta332@gmail.com>
2025-01-17 01:44:58 +0600

    done counter

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 const App = () => {
-  const handleClick = () => {
-    alert("Hello World!");
-  };
+  const [count, setCount] = useState(0);
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <button
-        onClick={handleClick}
-        className="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring focus:ring-blue-300 transition duration-300 "
-      >
-        Click Me
-      </button>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4">Counter App</h1>
+      <div className="text-5xl mb-6">{count}</div>
+      <div className="space-x-4">
+        <button
+          onClick={() => setCount(count + 1)}
+          className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={() => setCount(count - 1)}
+          className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => setCount(0)}
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 };
